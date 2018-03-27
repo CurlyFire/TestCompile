2ce1be82703de858c824cdbdbc723daa649f2ce7
CurlyFire <stephane.denomme@inspq.qc.ca>
2018-03-27 10:59:15 -0400

    Initial commit

---
+++ FullNet47WithPackagesConfig/Program.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace FullNet47WithPackagesConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new TestFixtureAttribute();
+        }
+    }
+}
---
+++ FullNet47WithPackagesReference/Program.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace FullNet47WithPackagesReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new TestFixtureAttribute();
+        }
+    }
+}
